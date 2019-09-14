40aa06de57fa219ce7e344665366cf5fb9fd9273
petern160 <peternguyen160@gmail.com>
2019-09-14 10:05:53 -0700

    wireframe readme added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-###
+### VanHack NPO wireframe
+
+https://gomockingbird.com/projects/lq8mrf5/4gXVnC
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { NavLink } from 'react-router-dom'; 
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
+      <NavLink to="/">Home </NavLink>
+      <NavLink to="/about">About </NavLink>
       <NavLink to="/contact">Contact</NavLink>
     </div>
   );
