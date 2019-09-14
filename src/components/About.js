c3b1fd39e027f0c5560693d60b7fe677a15bd900
petern160 <peternguyen160@gmail.com>
2019-09-14 16:40:42 -0700

    finished styling home page added button and color schemes

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "../css/About.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navigation from "./Navigation";
 import { NavLink } from "react-router-dom";
 
 const About = () => {
   return (
-    <div className="start hero-c">
-      <h1>test</h1>
-    </div>
+    <>
+      <div className="start hero-c">
+        <NavLink className="enter-diagram" to="/">
+          Click To Enter
+        </NavLink>
+        <div className="first-half">
+          test Culpa officia eiusmod adipisicing ut veniam amet id officia sit
+          consequat ipsum et ex nostrud. Et elit dolor mollit laboris. Ullamco
+          commodo cupidatat proident eu tempor cupidatat anim cillum eu ipsum
+          excepteur non ipsum. Sunt est laborum ipsum cupidatat cupidatat aute
+          velit magna non deserunt.
+        </div>
+      </div>
+    </>
   );
 };
 
