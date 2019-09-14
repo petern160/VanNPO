e7b7b1a59f4c985f092793982e8f1904033ed51f
petern160 <peternguyen160@gmail.com>
2019-09-14 09:57:45 -0700

    changed to functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./components/Home";
 import About from "./components/About";
@@ -7,22 +7,20 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import Navigation from "./components/Navigation";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Navigation />
-          <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route component={Error} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
+const App = props => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navigation />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route component={Error} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
-export default App; 
+export default App;
