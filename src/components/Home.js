f39ced0ed48da88a379209259363c01eacab30e3
petern160 <peternguyen160@gmail.com>
2019-09-15 12:58:55 -0700

    added in description of website on hompage

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,11 +14,15 @@ const Home = () => {
           Click To View Diagram
         </NavLink>
         <div className="first-half">
-          test Culpa officia eiusmod adipisicing ut veniam amet id officia sit
-          consequat ipsum et ex nostrud. Et elit dolor mollit laboris. Ullamco
-          commodo cupidatat proident eu tempor cupidatat anim cillum eu ipsum
-          excepteur non ipsum. Sunt est laborum ipsum cupidatat cupidatat aute
-          velit magna non deserunt.
+          <p className="about-paragraph">
+            The purpose of this project is to make the process of learning about
+            the most common types of cancer tumors with reliable medical info
+            from fact-checked and peer reviewed sources. The site allows for
+            interactive research and provid infomation on treatments and
+            preventive measures available. If you are in need to find a doctor
+            to consult in person, we offer functionality to find a medical
+            professional in your area for possible dianosis and treatments.
+          </p>
         </div>
       </div>
     </>
