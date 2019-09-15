da60b58d00325b46527992ef21ad08abd2962001
petern160 <peternguyen160@gmail.com>
2019-09-15 11:17:11 -0700

    created new nav bar and logo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../css/Contact.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
+import Navbar from "./Navbar.js";
 
 const Contact = props => {
   const [data, setData] = useState([]);
@@ -36,116 +37,125 @@ const Contact = props => {
           let list = res.data.data;
           // console.log(list);
 
-          let viable = [] // available nearby doctors
+          let viable = []; // available nearby doctors
 
           list.forEach(object => {
             // console.log(object);
             object.practices.forEach(practice => {
               // console.log(practice);
-                if( practice.within_search_area && practice.accepts_new_patients ){
-                  viable.push(practice)
+              if (
+                practice.within_search_area &&
+                practice.accepts_new_patients
+              ) {
+                viable.push(practice);
               }
             });
           });
-          
+
           console.log(viable);
           setData(viable);
-          
         });
     }
-  });  
+  });
   return (
-    <div className="LungCancer">
-      <h1 className="CancerTitle">Lung Cancer</h1>
-      <img className="LungImage" src="/img/lung.jpg" />
+    <>
+      <Navbar />
+      <div className="LungCancer">
+        <h1 className="CancerTitle">Lung Cancer</h1>
+        <img className="LungImage" src="/img/lung.jpg" />
 
-      <div className="LungCancerBody">
-        <h3>INTRO</h3>
-        <p>
-          Lung cancer is the leading cause of cancer death for both men and
-          women. About 26% of all cancer deaths are from lung cancer.
-        </p>
-        <p>
-          Lung Cancer is responsible for more deaths in Canada than breast,
-          prostate and colon cancer combined.
-        </p>
+        <div className="LungCancerBody">
+          <h3>INTRO</h3>
+          <p>
+            Lung cancer is the leading cause of cancer death for both men and
+            women. About 26% of all cancer deaths are from lung cancer.
+          </p>
+          <p>
+            Lung Cancer is responsible for more deaths in Canada than breast,
+            prostate and colon cancer combined.
+          </p>
 
-        <h3>CAUSES</h3>
-        <p>Tobacco smoking is the number one cause of lung cancer. (85%-90%)</p>
-        <p>Long term exposure to high concentrations of radon </p>
+          <h3>CAUSES</h3>
+          <p>
+            Tobacco smoking is the number one cause of lung cancer. (85%-90%)
+          </p>
+          <p>Long term exposure to high concentrations of radon </p>
 
-        <h3>SYMPTOMS</h3>
-        <p>
-          Early stages of lung cancer often have no symptoms. Because of this,
-          most lung cancer patients already have advanced disease by the time
-          they are diagnosed.
-        </p>
-        <p>Symptoms of advanced stages may include:</p>
-        <ul>
-          <li>
-            New, changing or worsening cough, especially if the sputum contains
-            blood
-          </li>
-          <li>
-            Change in the ability to perform exercise (e.g. shortness of breath
-            with walking)
-          </li>
-          <li>Repeated episodes of pneumonia and/or bronchitis</li>
-          <li>Fever, weakness, weight loss</li>
-          <li>Chest pain</li>
-          <li>Difficulty swallowing</li>
-          <li>Growth of lymph nodes in the neck</li>
-        </ul>
+          <h3>SYMPTOMS</h3>
+          <p>
+            Early stages of lung cancer often have no symptoms. Because of this,
+            most lung cancer patients already have advanced disease by the time
+            they are diagnosed.
+          </p>
+          <p>Symptoms of advanced stages may include:</p>
+          <ul>
+            <li>
+              New, changing or worsening cough, especially if the sputum
+              contains blood
+            </li>
+            <li>
+              Change in the ability to perform exercise (e.g. shortness of
+              breath with walking)
+            </li>
+            <li>Repeated episodes of pneumonia and/or bronchitis</li>
+            <li>Fever, weakness, weight loss</li>
+            <li>Chest pain</li>
+            <li>Difficulty swallowing</li>
+            <li>Growth of lymph nodes in the neck</li>
+          </ul>
 
-        <h3>PREVENTIONS</h3>
-        <p>Don’t smoke, and avoid exposure to tobacco and cigarette smoke. </p>
-        <p>
-          {" "}
-          Quitting will reduce your cancer risk. Your risk of lung cancer
-          decreases for each year that you haven't smoked. Ten years after
-          quitting, the risk of dying from lung cancer is about half that of a
-          person who is still smoking.
-        </p>
-        <p>
-          If you live in an area where radon gas is likely, consider ways to
-          reduce radon build up in your home.
-        </p>
-        <p>
-          Lung cancer screening using low-dose CT scans is currently recommended
-          by the Canadian Task Force on Preventive Health Care.{" "}
-        </p>
+          <h3>PREVENTIONS</h3>
+          <p>
+            Don’t smoke, and avoid exposure to tobacco and cigarette smoke.{" "}
+          </p>
+          <p>
+            {" "}
+            Quitting will reduce your cancer risk. Your risk of lung cancer
+            decreases for each year that you haven't smoked. Ten years after
+            quitting, the risk of dying from lung cancer is about half that of a
+            person who is still smoking.
+          </p>
+          <p>
+            If you live in an area where radon gas is likely, consider ways to
+            reduce radon build up in your home.
+          </p>
+          <p>
+            Lung cancer screening using low-dose CT scans is currently
+            recommended by the Canadian Task Force on Preventive Health Care.{" "}
+          </p>
 
-        <h3>TREATMENT</h3>
-        <ul>
-          <li>Surgery</li>
-          <li>Radiation Therapy</li>
-          <li>Chemotherapy</li>
-        </ul>
+          <h3>TREATMENT</h3>
+          <ul>
+            <li>Surgery</li>
+            <li>Radiation Therapy</li>
+            <li>Chemotherapy</li>
+          </ul>
 
-        <h3>REFERENCES</h3>
-        <a
-          href="https://bccancerfoundation.com/why-give/research/lung"
-          target="_blank"
-        >
-          BC Cancer Foundation
-        </a>
-        <br />
-        <a
-          href="http://www.bccancer.bc.ca/health-info/types-of-cancer/lung/lung"
-          target="_blank"
-        >
-          BC Cancer
-        </a>
+          <h3>REFERENCES</h3>
+          <a
+            href="https://bccancerfoundation.com/why-give/research/lung"
+            target="_blank"
+          >
+            BC Cancer Foundation
+          </a>
+          <br />
+          <a
+            href="http://www.bccancer.bc.ca/health-info/types-of-cancer/lung/lung"
+            target="_blank"
+          >
+            BC Cancer
+          </a>
+        </div>
+        {data.map(data => (
+          <h1 key={data.name}>
+            Name: {data.name}
+            Address: {data.visit_address.street}, {data.visit_address.city}
+            {/* <img className="profile-picture" src={data.profile.image_url} /> */}
+          </h1>
+        ))}
+        <button onClick={helpButton}>get help</button>
       </div>
-      {data.map(data => (
-        <h1 key={data.name}>
-          Name: {data.name}
-          Address: {data.visit_address.street}, {data.visit_address.city}
-          {/* <img className="profile-picture" src={data.profile.image_url} /> */}
-        </h1>
-      ))}
-      <button onClick={helpButton}>get help</button>
-    </div>
+    </>
   );
 };
 
